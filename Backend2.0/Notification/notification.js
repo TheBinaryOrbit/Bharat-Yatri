8d9e8bffb9aec5d7747e13358dbcfea75cce7433
SH <contactshivgupta@gmail.com>
2025-08-29 01:13:17 +0530

    hello

--- Backend2.0/Notification/notification.js
+++ Backend2.0/Notification/notification.js
@@ -2,40 +2,42 @@ import { firebaseadmin } from "../Firebase/firebase.js";
 import { User } from "../Model/UserModel.js";
 
 
-export const sendnotification = async (vehicleType, notification , bookedBy) => {
+export const sendnotification = async (vehicleType, notification, bookedBy) => {
+
+  const userarray = await getUserByvehicleType(vehicleType, bookedBy);
+
+  console.log("User array:", userarray);
+  if (userarray.length === 0) {
+    console.log("No users found for this vehicle type.");
+    return "No users found for this vehicle type.";
+  }
 
 
-    const userarray = await getUserByvehicleType(vehicleType , bookedBy);
-
-    console.log("User array:", userarray);
-    if (userarray.length === 0) {
-        console.log("No users found for this vehicle type.");
-        return "No users found for this vehicle type.";
-    }
-
-    
-    const message = {
-        notification: {
-            title: "Hello Rider",
-            body: `New ${vehicleType} ride added`
-        },
-        data: {
-            message: JSON.stringify({
-                carModel: vehicleType,
-                from: notification.pickUpLocation,
-                to: notification.dropLocation,
-            })
-        },
-        tokens: userarray
-    };
-    try {
-        const response = await firebaseadmin.messaging().sendEachForMulticast(message);
-        console.log("Message send sucessfully");
-        return "message send sucessfully"
-    } catch (error) {
-        console.log(error)
-        return "error in sending notification"
-    }
+  const message = {
+    notification: {
+      title: "Hello Rider",
+      body: `New ${vehicleType} ride added`
+    },
+    data: {
+      message: JSON.stringify({
+        carModel: vehicleType,
+        from: notification.pickUpLocation,
+        to: notification.dropLocation,
+      }),
+      action: "open_chat",
+      bookingId: bookedBy,
+      click_action: "FLUTTER_NOTIFICATION_CLICK",
+    },
+    tokens: userarray
+  };
+  try {
+    const response = await firebaseadmin.messaging().sendEachForMulticast(message);
+    console.log("Message send sucessfully");
+    return "message send sucessfully"
+  } catch (error) {
+    console.log(error)
+    return "error in sending notification"
+  }
 }
 
 
