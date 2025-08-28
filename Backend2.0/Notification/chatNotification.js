f8c857167578145b794dca80a8886ee59524636c
SH <contactshivgupta@gmail.com>
2025-08-29 01:54:36 +0530

    hello 6

--- Backend2.0/Notification/chatNotification.js
+++ Backend2.0/Notification/chatNotification.js
@@ -1,7 +1,7 @@
 import { firebaseadmin } from "../Firebase/firebase.js";
 import { User } from "../Model/UserModel.js";
 
-export const sendChatNotification = async (senderName, reciver, content) => {
+export const sendChatNotification = async (senderName , reciver , content) => {
 
     const token = await User.findById(reciver).select("fcmToken");
     console.log("Token:", token);
@@ -9,34 +9,10 @@ export const sendChatNotification = async (senderName, reciver, content) => {
     const tokenArray = token.fcmToken;
     const message = {
         notification: {
-            title: String(senderName ?? "New message"),
-            body: String(content ?? "Tap to open chat"),
-        },
-        data: {
-            action: "open_chat",
-            bookingId: String(bookingId ?? ""),      // required by your app
-            otherUserId: String(senderId ?? ""),     // the chat peer for the receiver
-            otherUserName: String(senderName ?? ""), // optional (nice for AppBar)
-            click_action: "FLUTTER_NOTIFICATION_CLICK",
+            title: senderName,
+            body: "testing"
         },
         tokens: [tokenArray],
-        android: {
-            priority: "high",
-            notification: {
-                channelId: "chat_channel",
-                sound: "default",
-            },
-        },
-        apns: {
-            headers: { "apns-priority": "10" },
-            payload: {
-                aps: {
-                    sound: "default",
-                    contentAvailable: true,
-                    mutableContent: true,
-                },
-            },
-        },
     };
     try {
         const response = await firebaseadmin.messaging().sendEachForMulticast(message);
